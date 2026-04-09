3447b0b2f6c6cd0e104f5220e9f35a777ea00431
sofya-ibrahimova <sofya.ibr@gmail.com>
2026-04-09 19:57:14 +0400

    added new line

--- src/lesson33/Lesson33.java
+++ src/lesson33/Lesson33.java
@@ -4,6 +4,7 @@ public class Lesson33 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("I am try to learning git!");
+        System.out.println("I am still learning ");
 
     }
 
