7edb71dff8082db415419029cc62db209a8edf48
sofya-ibrahimova <sofya.ibr@gmail.com>
2026-04-09 15:17:51 +0400

    message

--- src/lesson33/Lesson33.java
+++ src/lesson33/Lesson33.java
@@ -3,6 +3,7 @@ package lesson33;
 public class Lesson33 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("I am try to learning git!");
 
     }
 
