f3bf861e9db378d69ad4bf476418406b51666a48
sofya-ibrahimova <sofya.ibr@gmail.com>
2026-04-09 14:50:54 +0400

    comment

--- src/lesson33/Lesson33.java
+++ src/lesson33/Lesson33.java
@@ -3,7 +3,6 @@ package lesson33;
 public class Lesson33 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("I am leraning to use GIT!");
-    }
+    }пше
 
 }
