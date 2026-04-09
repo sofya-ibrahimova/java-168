810cbe353ba2bcfa839d1af0e99375bffc1e5d4f
sofya-ibrahimova <sofya.ibr@gmail.com>
2026-04-09 21:07:45 +0400

    added new method

--- src/lesson33/Lesson33.java
+++ src/lesson33/Lesson33.java
@@ -3,6 +3,11 @@ package lesson33;
 public class Lesson33 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
     }
 
 }
