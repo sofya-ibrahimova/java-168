0407fc66fe0b89c15408f19834789ba4409da31d
sofya-ibrahimova <sofya.ibr@gmail.com>
2026-04-10 12:43:37 +0400

    add two variables

--- src/lesson33/TestGitBranches.java
+++ src/lesson33/TestGitBranches.java
@@ -4,11 +4,13 @@ public class TestGitBranches {
     public static void main(String[] args) {
 
         int sum = 0;
-        for (int i = 0; i < 10; i++ ){
+        for (int i = 0; i < 10; i++) {
             sum += i;
         }
         System.out.println(sum);
 
         System.out.println("mybranch was created");
+        int a = 1;
+        int b = a * 3;
     }
 }
